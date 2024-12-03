23c1622226864457308864e948eb1e2bbb27bcbd
viweu <153850831+viweu@users.noreply.github.com>
2024-12-03 17:50:26 +0300

    Create ConnectData.java

---
+++ db/ConnectData.java
@@ -0,0 +1,11 @@
+package db;
+
+public class ConnectData {
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String DB = "registry";
+    public static final String URL = "jdbc:mysql://localhost:3306/" + DB;
+    public static final String USER = "root";
+    public static final String PASS = "1862754M";
+
+    public static final String TABLE = "animals";
+}
