80115e3f9eba77f71c71bf3cb7bf1727a1ce43a1
viweu <153850831+viweu@users.noreply.github.com>
2024-12-03 17:46:53 +0300

    Create Horse.java

---
+++ Animals/Horse.java
@@ -0,0 +1,16 @@
+package Animals;
+
+public class Horse extends PackAnimals{
+    public Horse(String name, String date) {
+        super(name, date);
+    }
+
+    @Override
+    public String toString() {
+        return "Horse {" +
+                "name = '" + super.getName() + '\'' +
+                ", commands = " + super.getCommands() +
+                ", dateOfBirth = '" + super.getDateOfBirth() + '\'' +
+                '}';
+    }
+}
